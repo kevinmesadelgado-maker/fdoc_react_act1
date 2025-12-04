e0c1b4a89d97f7c16bf87ebe017979bb1c57d4d8
Tu Nombre <tu.email@example.com>
2025-12-04 16:20:38 -0500

    refactor: mover estructura de app/ a src/app para organización del proyecto

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,53 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import SaludoBasico from "./componets/SaludoBasico"
+import SaludoDesestructurado from "./componets/SaludoDesestructurado";
+import PerfilConObjeto from './componets/PerfilConObjeto'
+import BotonConCallback from './componets/BotonConCallback.'
+import Contenedor from "./componets/ContenedorRenderProp";
+import CajaChildren from "./componets/CajaChildren";
+import SaludoConDefaultProps from "./componets/SaludoConDefaultProps";
+
+
+export default function page() {
+  const usuario = {
+    nombre: "Maria",
+    hobbies: ["leer", "caminar", "programar"],
+  };
+    
+ const manejarClick = () => {
+    alert("¡Botón clicado!");
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+  <div>
+      <h1>Aplicación principal</h1>
+
+      {/* Caso 1: Con entusiasmo */}
+      <SaludoBasico nombre = "Juan" entusiasta = {true} />
+
+      {/* Caso 2: Sin entusiasmo */}
+      <SaludoBasico nombre = "Maria" entusiasta={false} />
+
+      {/* Componente desestructurado */}
+      <SaludoDesestructurado nombre="Emiliano" edad={20} />
+
+      {/* Contenedor con contenido directo */}
+      <Contenedor contenido = {<p className="text-gray-800 text-lg font-medium">Hola, soy Emiliano </p>} />
+
+      {/* Contenedor con función render */}
+      <Contenedor render = {() => <h2 className="text-blue-600 text-xl font-semibold">Hola desde una función render </h2>} />
+
+      <PerfilConObjeto usuario={usuario} />
+
+      <BotonConCallback onClick={manejarClick} texto="Clic aquí" />
+
+    <CajaChildren/>
+    
+    <SaludoConDefaultProps/> 
+     <Contenedor titulo="Mi Contenedor">
+      <p>Este es el contenido dentro del contenedor.</p>
+      <button>Acción</button>
+    </Contenedor>
     </div>
-  );
-}
+  )
+} 
